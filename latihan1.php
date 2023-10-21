518983752932491b528e14291f4b17395fcc1596
fathaneeh <fathan14729@gmail.com>
2023-10-21 11:35:04 +0700

    latihan php

---
+++ latihan1.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title>wad_project2</title>
+</head>
+
+<body>
+    <h1>SELAMAT DATANG</h1>
+
+    <?php
+    $nama = "fathan";
+    $nama
+    ?>
+
+
+</body>
+
+</html>
